8aebc59df416e1305ee32e7b5333111b9d5305c8
magnet9805 <magnet9805@naver.com>
2023-11-27 23:24:12 +0900

    캡셔닝 seq2seq 구현 완료, cpu 메모리 이슈

--- train/models/decoder_seq.py
+++ train/models/decoder_seq.py
@@ -1,30 +1,11 @@
 import numpy as np
-from common.time_layers import *
-import torch
-<<<<<<< HEAD
-from torch import nn
-
-class DecoderSeq:
-    def __init__(self, output_dim, embed_size, hidden_size, num_layers):
-        O, E, H, N = output_dim, embed_size, hidden_size, num_layers
-
-        self.embed = nn.Embedding(O, E)
-        self.lstm = nn.LSTM(E, H, N, batch_first=True)
-        self.affine = nn.Linear(H, O)
-
-    def forward(self, input, hidden, cell):
-
-        out = self.embed.forward(input)
-        out, (hidden, cell) = self.lstm.forward(out, (hidden, cell))
-        prediction = self.affine.forward(out)
-        return prediction, hidden, cell
-=======
 import torch.nn as nn
 
 class DecoderSeq(nn.Module):
     def __init__(self, output_dim, embed_dim, hidden_dim, num_layers):
         super().__init__()
-
+        
+        # output_dim == vocab_size
         self.output_dim = output_dim
         self.embed_dim = embed_dim
         self.hidden_dim = hidden_dim
@@ -33,24 +14,23 @@ class DecoderSeq(nn.Module):
         self.embed = nn.Embedding(output_dim, self.embed_dim)
         self.lstm = nn.LSTM(self.embed_dim, self.hidden_dim, num_layers=self.num_layers, batch_first=True)
         self.affine = nn.Linear(self.hidden_dim, output_dim)
-        #### self.softmax = nn.LogSoftmax(dim=1)
+        self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input, hidden): # input : comments tensor / hidden : 이미지 feature
 
         # input = [batch_size]
 
         #input = [1, batch_size]
-        input = input.unsqueeze(0)
+        input = input.view(1,-1)
 
         # out = [1, batch_size, embed_dim]
         out = self.embed(input)
 
         # out = [batch_size, seq_len, hidden_dim]
+        # hidden = [num_layers, batch_size, hidden_size]
         out, hidden, _ = self.lstm(out, hidden)
 
-        #### out = self.softmax(self.affine(out[0]))
-
         # out = [batch_size, output_dim]
-        out = self.affine(out.squeeze(1))
-        return out, hidden
->>>>>>> bdc553db1447fb56c0c00b02b576ca48ff746855
+        prediction = self.softmax(self.affine(out[0]))
+        return prediction, hidden
+
--- train/models/seq2seq.py
+++ train/models/seq2seq.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+import torch
+import random
 
 class Seq2seq(nn.Module):
     def __init__(self, encoder, decoder, device):
@@ -9,11 +11,36 @@ class Seq2seq(nn.Module):
         self.device = device
 
 
-    def forward(self, xs, ts): # xs = 이미지, ts = 문장
-        decoder_xs, decoder_ts = ts[:, :-1], ts[:, 1:]
+    def forward(self, imgs, comments, teacher_forcing_ratio=0.5):
+        
+        batch_size = comments.shape[0]
+        seq_len = comments.shape[1]
+        vocab_size = self.decoder.output_dim
+        input = comments[:, 0]
+        target = comments[:,1:]
+        
+        outputs = torch.zeros(seq_len, batch_size, vocab_size).to(self.device)
 
-        h, _ = self.encoder.forward(xs)
-        score = self.decoder.forward(decoder_xs, h)
-        loss = self.softmax.forward(score, decoder_ts)
-        return loss
+        # imgfeature 뽑기 (디코더의 첫번째 h)
+        decoder_hidden, _ = self.encoder.forward(imgs)
+
+        decoder_hidden = decoder_hidden.unsqueeze(0).to(self.device) # (1, batch_size, hidden_size)
+
+        
+
+        for t in range(seq_len):
+            decoder_out, decoder_hidden = self.decoder(input, decoder_hidden).to(self.device)
+            outputs[t] = decoder_out
+            
+            # 확률 가장 높게 예측한 토큰
+            _, topi = decoder_out.argmax(1)
+
+            # 50% 확률로 다음 토큰값으로 예측값 or 실제값
+            teacher_force = random.random() < teacher_forcing_ratio
+            input = target[t] if teacher_force else topi
+            
+            return outputs # (seq_len, batch_size, output_size)
+
+        
+        
 
