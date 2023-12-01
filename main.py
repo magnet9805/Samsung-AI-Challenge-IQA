f53e5cc39f0cfdd40230fe769011fe0ccb8ba92d
magnet9805 <magnet9805@naver.com>
2023-12-01 18:36:21 +0900

    첫번째 모델 (encoder(resnet), decoder(seq2seq) 기반) 구현 완료 12011836

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import dataset as d
-from util.preprocessing import  *
+from util.preprocessing import *
 import multiprocessing
 import torch
 import torch.nn as nn
@@ -21,7 +21,6 @@ import pandas as pd
 from train.trainer import trainer
 
 
-
 def seed_everything(seed):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
@@ -33,18 +32,17 @@ def seed_everything(seed):
 
 
 def main():
-
     CFG = {
-        'IMG_SIZE':224,
-        'EPOCHS':1000, #Your Epochs,
-        'LR':1e-5, #Your Learning Rate,
-        'BATCH_SIZE': 32, #Your Batch Size,
-        'SEED':41,
-        'num_worker' : multiprocessing.cpu_count(),
-        'EARLY_STOP' : 10
+        'IMG_SIZE': 224,
+        'EPOCHS': 1000,  # Your Epochs,
+        'LR': 1e-5,  # Your Learning Rate,
+        'BATCH_SIZE': 32,  # Your Batch Size,
+        'SEED': 41,
+        'num_worker': multiprocessing.cpu_count(),
+        'EARLY_STOP': 10
     }
 
-    seed_everything(CFG['SEED']) # Seed 고정
+    seed_everything(CFG['SEED'])  # Seed 고정
 
     train_mean = (0.4194325, 0.3830166, 0.3490198)
     train_Std = (0.23905228, 0.2253936, 0.22334467)
@@ -52,6 +50,7 @@ def main():
     valid_mean = (0.4170096, 0.38036022, 0.34702352)
     valid_Std = (0.23896241, 0.22566794, 0.22329141)
 
+    all_data = pd.read_csv('./data/open/train.csv')
     train_data = pd.read_csv('./data/open/train_data.csv')
     valid_data = pd.read_csv('./data/open/valid_data.csv')
     test_data = pd.read_csv('./data/open/test_data.csv')
@@ -62,30 +61,44 @@ def main():
     train_dataset = d.CustomDataset(train_data, 'train', transform=train_transform)
     valid_dataset = d.CustomDataset(valid_data, 'valid', transform=valid_transform)
 
+    train_loader = DataLoader(train_dataset, batch_size=CFG['BATCH_SIZE'], shuffle=True, num_workers=CFG['num_worker'],
+                              pin_memory=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=CFG['BATCH_SIZE'], shuffle=True, num_workers=CFG['num_worker'],
+                              pin_memory=True)
 
+    all_comments = ' '.join(all_data['comments']).split()
+    vocab = set(all_comments)
+    vocab = ['<PAD>', '<SOS>', '<EOS>'] + list(vocab)
+    word2idx = {word: idx for idx, word in enumerate(vocab)}
+    idx2word = {idx: word for word, idx in word2idx.items()}
 
-    train_loader = DataLoader(train_dataset, batch_size=CFG['BATCH_SIZE'], shuffle=True,num_workers=CFG['num_worker'], pin_memory=True)
-    valid_loader = DataLoader(valid_dataset, batch_size=CFG['BATCH_SIZE'], shuffle=True, num_workers=CFG['num_worker'], pin_memory=True)
+    hidden_dim = 512
+    embed_dim = 256
+    output_dim = len(vocab)
+    num_layers = 1
+    encoder = EncoderResnet(hidden_dim)
+    decoder = DecoderSeq(output_dim, embed_dim, hidden_dim, num_layers)
+    model = Seq2seq(encoder, decoder, word2idx, device)
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else "cpu")
+
+    criterion_mos = nn.MSELoss()
+    criterion_caption = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-5)
+
+    criterion_mos.to(device)
+    criterion_caption.to(device)
+    encoder.to(device)
+    decoder.to(device)
+    model.to(device)
 
     dataloader_dict = {'train': train_loader, 'valid': valid_loader}
+    criterion_dict = {'mos': criterion_mos, 'caption': criterion_caption}
 
-    encoder = EncoderResnet(512)
-    #torch.cuda.get_device_name(0)
-    device = torch.device('cuda:0' if torch.cuda.is_available() else "cpu")
-    print(device)
-    torch.cuda.is_available()
-    print(encoder)
-
-
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(encoder.parameters(), lr=1e-5)
-    criterion.to(device)
-    encoder.to(device)
-
-    train_history, valid_history = trainer(encoder, dataloader_dict=dataloader_dict, criterion=criterion, num_epoch=CFG['EPOCHS'], optimizer=optimizer, device=device, early_stop=CFG['EARLY_STOP'])
+    train_history, valid_history = trainer(model, dataloader_dict=dataloader_dict, criterion=criterion_dict,
+                                           num_epoch=CFG['EPOCHS'], optimizer=optimizer, device=device,
+                                           early_stop=CFG['EARLY_STOP'])
     return train_history, valid_history
-
-
 
 
 if __name__ == "__main__":
@@ -93,4 +106,4 @@ if __name__ == "__main__":
 
     pd = pd.DataFrame(columns=['train_loss', 'test_loss'],
                       data=[(train, valid) for train, valid in zip(train_history, valid_history)])
-    pd.to_csv('loss.csv')
+    pd.to_csv('loss.csv', encoding='cp949', index=False)
