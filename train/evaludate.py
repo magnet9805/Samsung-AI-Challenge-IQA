90f8dd9426d50901523b732f06e9b27c6aea3a4d
magnet9805 <magnet9805@naver.com>
2023-12-02 11:39:24 +0900

    오류 수정

--- train/evaludate.py
+++ train/evaludate.py
@@ -1,31 +1,31 @@
 import torch
-def evaluate(model, test_dataloader, criterion_dict, device, word2idx):
+from tqdm import tqdm
+
+
+def train(model, train_dataloader, optimizer, criterion_dict, device, word2idx):
     epoch_loss = 0
-    epoch_acc = 0
-    mask = 0
 
-    model.eval()
-    with torch.no_grad():
-        for img, mos, comment in test_dataloader:
-            img = img.to(device)
-            mos = mos.to(device)
-            comment = comment.to(device)
+    model.train()
+    for img, mos, comment in tqdm(train_dataloader):
+        img = img.to(device)
+        mos = mos.to(device)
 
-            comments_tensor = torch.zeros((len(comment), len(max(comment, key=len)))).long().to(device)
-            for i, comment in enumerate(comment):
-                tokenized = ['<SOS>'] + comment.split() + ['<EOS>']
-                comments_tensor[i, :len(tokenized)] = torch.tensor([word2idx[word] for word in tokenized])
+        comments_tensor = torch.zeros((len(comment), len(max(comment, key=len)))).long().to(device)
+        for i, comment in enumerate(comment):
+            tokenized = ['<SOS>'] + comment.split() + ['<EOS>']
+            comments_tensor[i, :len(tokenized)] = torch.tensor([word2idx[word] for word in tokenized])
+        
+        predicted_caption, predicted_mos = model(img, comments_tensor)
+        caption_target = comments_tensor[:, 1:]
 
-            predicted_caption, predicted_mos = model(img, comment)
-            caption_target = comments_tensor[:,1:]
+        loss_mos = criterion_dict['mos'](predicted_mos.to(torch.float64), mos.to(torch.float64))
+        loss_caption = criterion_dict['caption'](predicted_caption.view(-1, predicted_caption.size(-1)), caption_target.reshape(-1))
+        loss = loss_mos + loss_caption
 
-            loss_mos = criterion_dict['mos'](predicted_mos.to(torch.float64), mos.to(torch.float64))
-            loss_caption = criterion_dict['caption'](predicted_caption.view(-1, cs.size(-1)), caption_target.reshape(-1))
-            loss = loss_mos + loss_caption
-            epoch_loss += loss.item()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-            mask += 1
-            if mask == 100:
-                break
+        epoch_loss += loss.item()
 
-    return epoch_loss / len(test_dataloader)
+    return epoch_loss / len(train_dataloader)
--- train/train.py
+++ train/train.py
@@ -9,18 +9,17 @@ def train(model, train_dataloader, optimizer, criterion_dict, device, word2idx):
     for img, mos, comment in tqdm(train_dataloader):
         img = img.to(device)
         mos = mos.to(device)
-        comment = comment.to(device)
 
         comments_tensor = torch.zeros((len(comment), len(max(comment, key=len)))).long().to(device)
         for i, comment in enumerate(comment):
             tokenized = ['<SOS>'] + comment.split() + ['<EOS>']
             comments_tensor[i, :len(tokenized)] = torch.tensor([word2idx[word] for word in tokenized])
-
-        predicted_caption, predicted_mos = model(img, comment)
+        
+        predicted_caption, predicted_mos = model(img, comments_tensor)
         caption_target = comments_tensor[:, 1:]
 
         loss_mos = criterion_dict['mos'](predicted_mos.to(torch.float64), mos.to(torch.float64))
-        loss_caption = criterion_dict['caption'](predicted_caption.view(-1, cs.size(-1)), caption_target.reshape(-1))
+        loss_caption = criterion_dict['caption'](predicted_caption.view(-1, predicted_caption.size(-1)), caption_target.reshape(-1))
         loss = loss_mos + loss_caption
 
         optimizer.zero_grad()
